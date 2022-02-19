266f63f6f26cbbfe18df039424fd9d5642329da4
Laveena1208 <2019laveena.kithani@ves.ac.in>
2022-02-19 20:22:04 +0530

    updated

--- INVICTUS_S16_VFAS/events/models.py
+++ INVICTUS_S16_VFAS/events/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-class Events(models.Model):
+from django.utils.text import slugify
+class Event(models.Model):
+    slug = models.SlugField(default='', editable=False, max_length=500)
     event_name = models.CharField(max_length=100)
     event_date = models.DateField()
     event_time = models.TimeField()
@@ -7,4 +9,12 @@ class Events(models.Model):
     second_year = models.BooleanField()
     third_year = models.BooleanField()
     fourth_year = models.BooleanField()
-# Create your models here.
+    upload_image = models.ImageField(upload_to ='uploads/',default='uploads/i1.jpeg' )
+
+    def save(self, *args, **kwargs):
+        value = slugify(self.event_name)
+        self.slug = value
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.event_name
