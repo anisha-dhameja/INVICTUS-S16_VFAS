a42b5ed18184ba7aee3a8cc986310ecd627eb325
khushboobajaj25 <khushboobajaj2572002@gmail.com>
2022-02-19 17:30:51 +0530

    feedback models implemented

--- INVICTUS_S16_VFAS/feedbacks/models.py
+++ INVICTUS_S16_VFAS/feedbacks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from events import event_models
+from accounts import user_models
+# Create your models here
+class FeedBackQuestions(models.Model):
+    questions = models.CharField(max_length=100)
+    events = models.ForeignKey(event_models.Events, on_delete=models.CASCADE, related_name="questions")
+    
 
-# Create your models here.
+class FeedBackResponse(models.Model):
+    user = models.ManyToManyField(user_models.User, on_delete=models.CASCADE, related_name="user")
+    questions = models.ForeignKey(FeedBackQuestions, on_delete=models.CASCADE)
+    response = models.CharField(max_length=100)
