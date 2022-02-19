9682d76c2eb05bda361f261f6d06b011f337d598
karan-rohra <2019karan.rohra@ves.ac.in>
2022-02-19 17:54:41 +0530

    Added register and login templates

--- INVICTUS_S16_VFAS/accounts/views.py
+++ INVICTUS_S16_VFAS/accounts/views.py
@@ -1,3 +1,12 @@
+from re import template
 from django.shortcuts import render
 
-# Create your views here.
+
+def register(request):
+    template_name = "accounts/register.html"
+    return render(request, template_name)
+
+
+def login(request):
+    template_name = "accounts/login.html"
+    return render(request, template_name)
