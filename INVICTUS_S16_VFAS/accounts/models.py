a18846a42e55527a86093c9b217e4bebc52c66be
anishadhameja <68025041+anishadhameja@users.noreply.github.com>
2022-02-19 17:23:37 +0530

    initial commit

--- INVICTUS_S16_VFAS/accounts/models.py
+++ INVICTUS_S16_VFAS/accounts/models.py
@@ -1,3 +1,18 @@
+from asyncio import events
+from re import T
 from django.db import models
+from django.contrib.auth import models as auth_models
+from feedbacks import models as feedback_models
+from events import models as events_models
 
 # Create your models here.
+
+
+class User(auth_models.AbstractUser):
+    email = models.EmailField(max_length=300, unique=True)
+    USERNAME_FIELD = "email"
+    username = None
+    questions = models.ManyToManyField(
+        feedback_models.FeedbackQuestions)
+    events = models.ManyToManyField(events_models.Events)
+    branch = models.CharField(max_length=200)
