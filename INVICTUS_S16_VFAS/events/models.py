30a124f937a91d0b8461757c86aacfc0f23a63ea
Laveena1208 <2019laveena.kithani@ves.ac.in>
2022-02-19 17:30:38 +0530

    update events model

--- INVICTUS_S16_VFAS/events/models.py
+++ INVICTUS_S16_VFAS/events/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+class Events(models.Model):
+    event_name = models.CharField(max_length=100)
+    event_date = models.DateField()
+    event_time = models.TimeField()
+    first_year = models.BooleanField()
+    second_year = models.BooleanField()
+    third_year = models.BooleanField()
+    fourth_year = models.BooleanField()
 # Create your models here.
